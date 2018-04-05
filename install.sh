0aa61e50d14da4d0ec1bc27c1ed71a6ca8a486ec
Kahina Akretche <kahina@kahina-Lenovo-B590>
2018-04-05 18:55:06 +0100

    update install.sh

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash 
+
+
+INITDEV=""
+CMD=""
+
+
+#on se deplace dans le home de l'utilisateur
+cd /home/"$USER"/ 
+
+
+#On test si le répertoire .initdev existe deja
+if [ -e ".initdev" ] ;then 
+         echo "the reperetory already existes"
+         exit 1
+fi 
+
+
+#création du répertoire .initdev
+mkdir ".initdev"
+
+
+
+#copier les fichiers gitignores,sources,licenses,makefiles,bin dans .initdev
+
+echo "copiying the files gitgnores,sources,licenses,Makefiles in .initdev"
+cp "~/Téléchargements/gitignores"   "~/.initdev/gitignores"
+cp "~/Téléchargements/bin"          "~/.initdev/bin"
+cp "~/Téléchargements/sources"      "~/.initdev/Sources" 
+cp "~/Téléchargements/licenses"     "~/.initdev/Licences"
+cp "~/Téléchargements/makefiles"    "~/.initdev/Makefiles"
+
+
+
+#sauvegarder la modification de la variable PATH dans le fichier .bashrc si l'utilisateur utilise bash 
+
+if [ -e .bashrc ]; then
+
+                 PATH="$PATH:/home/"$USER"/.initdev"
+                 export PATH
+                 CMD=`which initdev`  
+
+          #vérification si l'alias initdev de l'app existe déja sinon le repmlacer par initdev2
+          if [ CMD=""]; then
+
+                     alias initdev= 'bash /home/"$USER"/.initdev/main.sh' >> ~/.bashrc
+                     source ~/.bashrc
+                     ALS="initdev"
+         else 
+                    alias initdev2= 'bash /home/"$USER"/.initdev/main.sh' >> ~/.bashrc
+                    source ~/.bashrc
+                    ALS="initdev2"
+          fi
+
+                 INITDEV="$PATH:/home/"$USER"/.initdev"
+                 export INITDEV
+fi
+
+#sauvegarder la modification de la variable PATH dans le fichier .bashrc si l'utilisateur utilise zsh
+
+if [ -e .zsh ]; then
+
+                 PATH="$PATH:/home/"$USER"/.initdev"
+                 export PATH
+                 CMD=`which initdev`
+
+           #vérification si l'alias initdev de l'app existe déja sinon le repmlacer par initdev2
+           if [ CMD=""]; then
+
+                     alias initdev= 'bash /home/"$USER"/.initdev/main.sh' >> ~/zshrc
+                     source ~/.zshrc
+                     ALS="initdev"
+         else 
+                    alias initdev2= 'bash /home/"$USER"/.initdev/main.sh' >> ~/.zshrc
+                    source ~/.zshrc
+                    ALS="initdev2"
+          fi
+               
+
+                 INITDEV="$PATH:/home/"$USER"/.initdev"
+                 export INITDEV
+fi
+echo " initdev installed successfully use the commande $ALS to execute the application"
+
+exit 0
